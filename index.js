0d37e4fd44edf069abdf69bf3249bb87ecc13c42
ang <tuanan298@icloud.com>
2024-06-18 17:26:03 +0700

    refactor: Remove unused userDAO class

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { applicationDefault, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import express from "express";
-import { userDAO } from "./classy.js";
+
 import cors from "cors";
 import { configDotenv } from "dotenv";
 const app = initializeApp(applicationDefault());
